5cd6b15852274b739429f261d2a3d85ee13ebc40
Stephen Choo <stephen.w.choo@gmail.com>
2023-02-13 19:53:55 +1100

    Renamed more things for AWS

--- application.py
+++ application.py
@@ -13,9 +13,7 @@ headers = {
     'Authorization': os.getenv("AUTHORIZATION")
 }
 
-application = Flask(__name__, static_folder='frontend/build', static_url_path='/')
-
-app = application
+application = app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
 
 # CORS(app)
 
