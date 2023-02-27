68a4ecd54c8dd88a0736192f92d13ec672a53ea5
Stephen Choo <stephen.w.choo@gmail.com>
2023-02-27 21:26:32 +1100

    Basic word cloud ended up using the relatively basic text version. Will add sentiment analysis next

--- data_processing.py
+++ data_processing.py
@@ -11,11 +11,11 @@ def word_frequency(text):
 
 def top_twenty_words(word_freq):
     # Takes a dictionary of word frequencies and returns a list of the top 20 words
-    return sorted(word_freq, key=word_freq.get, reverse=True)[:20]
+    return sorted(word_freq, key=word_freq.get, reverse=True)[:25]
 
 def word_cloud(text):
     # Takes a string and returns a list of the top 20 words in the format required for word cloud
     # [{"text": "word", "value": 10}, ...]
     word_freq = word_frequency(text)
     top_words = top_twenty_words(word_freq)
-    return [{"text": word, "value": word_freq[word]} for word in top_words]
+    return [{"value": word, "count": word_freq[word]} for word in top_words]
--- frontend/src/components/WordCloud.js
+++ frontend/src/components/WordCloud.js
@@ -6,35 +6,35 @@ import { schemeCategory10 } from 'd3-scale-chromatic';
 
 
 function WordCloudBox({wordCloud}) {
-  // const options = {
-  //   luminosity: 'dark',
-  //   hue: 'blue'
-  // }
+  const options = {
+    luminosity: 'dark',
+    hue: 'green'
+  }
 
-  const schemeCategory10ScaleOrdinal = scaleOrdinal(schemeCategory10)
+  // const schemeCategory10ScaleOrdinal = scaleOrdinal(schemeCategory10)
 
   return (
-    // <TagCloud
-    //   minSize={12}
-    //   maxSize={35}
-    //   tags={wordCloud}
-    //   colorOptions={options}
-    //   shuffle={true}
-    // />
-    <WordCloud
-    data={wordCloud}
-    width={200}
-    height={200}
-    font="Times"
-    fontStyle="italic"
-    fontWeight="bold"
-    fontSize={(word) => Math.log2(word.value) * 5}
-    spiral="rectangular"
-    rotate={(word) => word.value % 360}
-    padding={5}
-    random={Math.random}
-    fill={(d, i) => schemeCategory10ScaleOrdinal(i)}
-  />
+    <TagCloud
+      minSize={12}
+      maxSize={35}
+      tags={wordCloud}
+      colorOptions={options}
+      shuffle={true}
+    />
+    // <WordCloud
+    // data={wordCloud}
+    // width={200}
+    // height={200}
+    // font="Times"
+    // fontStyle="italic"
+    // fontWeight="bold"
+    // fontSize={(word) => Math.log2(word.value) * 3}
+    // spiral="rectangular"
+    // rotate={(word) => word.value % 360}
+    // padding={2}
+    // random={Math.random}
+    // fill={(d, i) => schemeCategory10ScaleOrdinal(i)}
+  // />
   )
 }
 
