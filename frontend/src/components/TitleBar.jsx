09ce3f6d5dadc3285cb4f2d22345fa12ab49465d
Stephen Choo <stephen.w.choo@gmail.com>
2023-03-07 20:48:52 +1100

    Adjusted visual glitches, rearranged some things for aesthetics

--- frontend/src/components/TitleBar.jsx
+++ frontend/src/components/TitleBar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { Box, Button, Flex, Spacer, Link, Icon, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Link, Icon, Image, Text, Heading } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
+import Logo from "../images/logo.png";
 
 function TitleBar(props) {
   return (
     <div>
       <Box backgroundColor={"#61777f"}>
         <Flex maxW={"100ch"} margin={"0 auto"} justifyContent={"space-between"} alignItems={"center"}>
-          <Link href="/">
-            <img className="logo" src={require("../images/logo.png")} alt="logo"/>
-          </Link>
+          <Flex alignItems="center">
+            <Link href="/">
+              <Image src={Logo} h="80px" display={"inline-block"}/>
+            </Link>
+            <Box
+              display="inline-block"
+            >
+              <Heading color="whiteAlpha.800" letterSpacing={"tight"} fontFamily={"Montserrat"} mb={-2}>
+                Markov
+              </Heading>
+              <Heading color="whiteAlpha.800" letterSpacing={"tight"} fontFamily={"Montserrat"} ml={"2px"} mt={-2}>
+                Twitter
+              </Heading>
+            </Box>
+          </Flex>
 
           <Link href="https://github.com/stephen-w-choo/markov-twitter" m={3}>
             <Button colorScheme={"teal"}>
